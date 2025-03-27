4771690d34a3c710195544cc8b6762e4e26a2819
Noajt <noaj.t.93@gmail.com>
2025-03-27 14:28:53 -0400

    Implement sqlite3 database

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 import requests
 import selectorlib
+import sqlite3
 from send_email import send_email
 
 "INSERT INTO events VALUES ('Tigers', 'Tiger City', '2099.10.15')"
@@ -10,6 +11,9 @@ URL = "https://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36'
                   ' (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+# Establish connection and cursor
+connection = sqlite3.connect("data.db")
 
 def scrape(url):
     """Scrape page source from URL"""
@@ -23,21 +27,31 @@ def extract(source):
     return value
 
 def store(extracted):
-    with open("data.txt", "a") as file:
-        file.write(extracted + "\n")
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+    connection.commit()
 
 def read(extracted):
-    with open("data.txt", "r") as file:
-        return file.read()
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    band, city, date = row
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
+    rows = cursor.fetchall()
+    print(rows)
+    return rows
 
 if __name__ == "__main__":
     while True:
         scraped = scrape(URL)
         extracted = extract(scraped)
         print(extracted)
-        content = read(extracted)
+
         if extracted != "No upcoming tours":
-            if extracted not in content:
+            row = read(extracted)
+            if not row:
                 store(extracted)
                 send_email(message="Hey, a new event was found!")
         time.sleep(2)
